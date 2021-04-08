75f59a1ba20f43794af6de59a5bb03ce5fb8710d
Connor Gregory Birzes <tuh16611@temple.edu>
2021-04-07 20:20:05 -0400

    Comic and Tag classes both filled in and ready for use.

--- ComicLibrary/Comic.cs
+++ ComicLibrary/Comic.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ComicLibrary
 {
-    class Comic
+    public class Comic
     {
+        private String title;
+        private String creators;
+        private String description;
+        private float retailPrice;
+        private float resalePrice;
+        private DateTime releaseDate;
+        private int sellerId;
+        private int buyerId;
+
+        public String Title { get { return title; } set { title = value; } }
+        public String Creators { get { return creators; } set { creators = value; } }
+        public String Description { get { return description; } set { description = value; } }
+        public float RetailPrice { get { return retailPrice; } set { retailPrice = value; } }
+        public float ResalePrice { get { return resalePrice; } set { resalePrice = value; } }
+        public DateTime ReleaseDate { get { return releaseDate; } set { releaseDate = value; } }
+        public int SellerId { get { return sellerId; } set { sellerId = value; } }
+        public int BuyerId { get { return buyerId; } set { buyerId = value; } }
     }
 }
